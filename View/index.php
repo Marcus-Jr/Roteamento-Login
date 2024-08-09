03c5f9555946675e3af6a4f62c7122ec1870ec89
Marcus Jr. <133683002+ForgeSolucoesEstagio@users.noreply.github.com>
2024-08-09 10:09:02 -0300

    Create index.php

---
+++ View/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div>
+        <h1>Login</h1>
+        <form>
+            <div>
+                <label for="usuario">Usuário</label>
+                <input type="text" id="usuario" name="usuario">
+            </div>
+            <div>
+                <label for="senha">Senha</label>
+                <input type="text" id="senha" name="senha">
+            </div>
+            <div>
+                <button type="submit">Entrar</button>
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
